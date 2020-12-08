b2b8f1166cccd6078dc2dccb660fdbac3602fa4b
Mark_Rumyantsev <mrk.ru@list.ru>
2020-12-08 11:10:21 +0300

    web_project

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zadfcr5qhr+$fj5#=pswpc5@m1g_9dyrt6%c6w=zsc-31wsck+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
